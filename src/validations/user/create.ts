94b105478c7709395d2b84ee15ab9ff1eabeb19f
viniciusjacik <viniciusferreira@genz.com.py>
2023-10-03 16:25:04 -0300

    Fix build

---
+++ src/validations/user/create.ts
@@ -0,0 +1,15 @@
+import { celebrate } from 'celebrate';
+import Joi from 'joi';
+
+export const userCreateValidation = celebrate({
+	body: Joi.object().keys({
+		password: Joi.string().required().min(8),
+		name: Joi.string().required(),
+		surname: Joi.string().required(),
+		document: Joi.string().required(),
+		phone_number: Joi.string().required(),
+		whats: Joi.string(),
+		email: Joi.string().required().email(),
+		birthdate: Joi.date().required(),
+	}),
+});
